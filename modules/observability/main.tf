7113eaea910d631c87f6669ba0d7935efdcf88bf
HARKAOUI Hajar <105939774+hajargithub@users.noreply.github.com>
2026-09-14 00:25:58 +0100

    limit Azure monitoring daily ingestion

--- modules/observability/main.tf
+++ modules/observability/main.tf
@@ -4,14 +4,16 @@ resource "azurerm_log_analytics_workspace" "this" {
   resource_group_name = var.resource_group_name
   sku                 = "PerGB2018"
   retention_in_days   = var.retention_in_days
+  daily_quota_gb      = 0.1
   tags                = var.tags
 }
 
 resource "azurerm_application_insights" "this" {
-  name                = "appi-${var.name}"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  workspace_id        = azurerm_log_analytics_workspace.this.id
-  application_type    = "web"
-  tags                = var.tags
+  name                 = "appi-${var.name}"
+  location             = var.location
+  resource_group_name  = var.resource_group_name
+  workspace_id         = azurerm_log_analytics_workspace.this.id
+  application_type     = "web"
+  daily_data_cap_in_gb = 0.1
+  tags                 = var.tags
 }
