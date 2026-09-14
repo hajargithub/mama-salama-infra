af862c8c46b2efe8dd1bb3ed246193c1b40c43c0
HARKAOUI Hajar <105939774+hajargithub@users.noreply.github.com>
2026-09-14 03:10:25 +0100

    configure PostgreSQL access for Azure services

--- modules/postgres/main.tf
+++ modules/postgres/main.tf
@@ -1,15 +1,16 @@
 resource "azurerm_postgresql_flexible_server" "this" {
-  name                   = var.name
-  resource_group_name    = var.resource_group_name
-  location               = var.location
-  version                = var.postgres_version
-  administrator_login    = var.administrator_login
-  administrator_password = var.administrator_password
-  sku_name               = var.sku_name
-  storage_mb             = var.storage_mb
-  backup_retention_days  = 7
-  zone                   = "1"
-  tags                   = var.tags
+  name                          = var.name
+  resource_group_name           = var.resource_group_name
+  location                      = var.location
+  version                       = var.postgres_version
+  administrator_login           = var.administrator_login
+  administrator_password        = var.administrator_password
+  sku_name                      = var.sku_name
+  storage_mb                    = var.storage_mb
+  backup_retention_days         = 7
+  zone                          = "1"
+  public_network_access_enabled = true
+  tags                          = var.tags
 }
 
 resource "azurerm_postgresql_flexible_server_database" "this" {
@@ -23,4 +24,10 @@ resource "azurerm_postgresql_flexible_server_configuration" "ssl" {
   name      = "require_secure_transport"
   server_id = azurerm_postgresql_flexible_server.this.id
   value     = "on"
+}
+resource "azurerm_postgresql_flexible_server_firewall_rule" "azure_services" {
+  name             = "AllowAzureServices"
+  server_id        = azurerm_postgresql_flexible_server.this.id
+  start_ip_address = "0.0.0.0"
+  end_ip_address   = "0.0.0.0"
 }
